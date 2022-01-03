91c7c56228122e50e453593ef2b7946d09e4d856
konnArrr <konnar1and0@gmail.com>
2022-01-03 19:59:37 +0100

    title label inserted

---
+++ GetItDone/GDGradient.swift
@@ -0,0 +1,51 @@
+//
+//  GDGradient.swift
+//  GetItDone
+//
+//  Created by Konstantin Schirmer on 03.01.22.
+//
+
+import Foundation
+import UIKit
+
+
+class GDGradient: UIView {
+    
+//    var colors: [CGColor] = [
+//        UIColor.init(red: 100, green: 228, blue: 255).cgColor,
+//        UIColor.init(red: 100, green: 123, blue: 213).cgColor
+//    ]
+    
+    var hexColors: [CGColor] = [
+        UIColor.blueZero.cgColor,
+        UIColor.blueOne.cgColor
+    ]
+    
+
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let layer = self.layer as? CAGradientLayer {
+            layer.colors = self.hexColors
+            layer.locations = [0.0, 1.2]
+        } else {
+            print("is not gradient")
+        }
+        
+    }
+    
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
--- GetItDone/WelcomeController.swift
+++ GetItDone/WelcomeController.swift
@@ -11,12 +11,12 @@ class WelcomeController: UIViewController {
     
     
     let bg: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .blue
+        let view = GDGradient()
         view.layer.cornerRadius = 6
         return view
     }()
+    
+    let titleLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +26,20 @@ class WelcomeController: UIViewController {
         view.addSubview(bg)
         
         bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        bg.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        bg.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        
+        bg.addSubview(titleLabel)
+        titleLabel.text = "GET IT DONE"
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: bg.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: bg.topAnchor, constant: 60).isActive = true
+        
         
     }
     
